fd89b4483f1d69492edaf1d2219709a94b4565f0
michaelBedDev <micama2000@gmail.com>
2023-10-16 09:45:43 +0200

    Creacion ejercicio Perrito

---
+++ ud5/Perrito.java
@@ -0,0 +1,24 @@
+public class Perrito{
+
+    String nome;
+    String raza;
+    String color;
+    boolean chip;
+    int numChip;
+    
+    public void ladrar() {
+        System.out.println("Guau guau");
+    
+    }
+    
+    public void durmir() {
+        System.out.println("Marcho durmir");
+    }
+    
+    public void comer() {
+        System.out.println("Guau guau Guau guau Guau guau Guau guau Guau guau");
+    
+    }
+    
+    
+    }
---
+++ ud5/TestPerrito.java
@@ -0,0 +1,17 @@
+public class TestPerrito {
+    public static void main(String[] args) {
+        
+        Perrito Bobby = new Perrito();
+        Perrito Lassie = new Perrito();
+        Perrito Beethoven = new Perrito();
+    
+        Bobby.ladrar();
+        Lassie.ladrar();
+        Beethoven.ladrar();
+    
+        Beethoven.comer();
+        Beethoven.durmir();
+
+
+    }
+}
