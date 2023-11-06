bc7a1a2854b1a4b9347e3e0ccbb22a67df5d3fe2
michaelBedDev <micama2000@gmail.com>
2023-11-06 10:12:55 +0100

    Calculadora Avanzada

--- ud5/appCalculadora/CalculadoraAvanzada.java
+++ ud5/appCalculadora/CalculadoraAvanzada.java
@@ -1,12 +1,14 @@
 package cd2023.ud5.appCalculadora;
 
+import java.lang.Math;
+
 public class CalculadoraAvanzada extends Calculadora {
     public void raíz() {
-        this.resultado = Math.sqrt(resultado);
+        this.resultado = Math.sqrt(this.resultado);
     }
 
     public void cuadrado() {
-        this.resultado = Math.pow(resultado, 2);
+        this.resultado = Math.pow(this.resultado, 2);
 
     }
 }
