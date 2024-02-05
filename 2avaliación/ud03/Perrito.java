0076fb97665d62d22f397f204e11cbad2312b77a
michaelBedDev <micama2000@gmail.com>
2024-02-05 09:22:00 +0100

    First commit for junit

---
+++ 2avaliación/ud03/Perrito.java
@@ -0,0 +1,26 @@
+package ud5;
+
+public class Perrito{
+
+    String nome;
+    String raza;
+    String color;
+    boolean chip;
+    int numChip;
+
+    public String ladrar() {
+        println "Guau guau";
+
+    }
+
+    public String durmir() {
+        return "Marcho durmir";
+    }
+
+    public void comer() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println("Guau guau");
+        }
+    }
+}
