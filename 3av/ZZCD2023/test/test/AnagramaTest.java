9a5dd0c91ffc63cfae9a327d154db1e46b22614b
michaelBedDev <micama2000@gmail.com>
2024-04-14 23:33:01 +0200

    Añadido test parametrizado

--- 3av/ZZCD2023/test/test/AnagramaTest.java
+++ 3av/ZZCD2023/test/test/AnagramaTest.java
@@ -3,6 +3,8 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import ejercicio2.Anagrama;
 
@@ -14,26 +16,51 @@ class AnagramaTest {
 		
 		assertTrue(anagrama.comprobarAnagrama("roma", "amor"));
 		
-		assertFalse(anagrama.comprobarAnagrama("roma", "amorr"));
-		
 		assertTrue(anagrama.comprobarAnagrama("copia", "opiac"));
-
-		assertFalse(anagrama.comprobarAnagrama("roma", "romA"));
-
+		
 		assertTrue(anagrama.comprobarAnagrama("arroz", "zorra"));
+
+		assertTrue(anagrama.comprobarAnagrama("casa", "saca"));
 
 		assertTrue(anagrama.comprobarAnagrama("snake", "ekans"));
 
-        assertTrue(anagrama.comprobarAnagrama("pato", "topa"));
+		assertTrue(anagrama.comprobarAnagrama("pato", "topa"));
+
+		assertTrue(anagrama.comprobarAnagrama("caso", "saco"));
         
-        assertTrue(anagrama.comprobarAnagrama("casa", "saca"));
+		assertFalse(anagrama.comprobarAnagrama("roma", "amorr"));
         
-        assertTrue(anagrama.comprobarAnagrama("caso", "saco"));
+		assertFalse(anagrama.comprobarAnagrama("roma", "romA"));
         
 		assertFalse(anagrama.comprobarAnagrama("albóndiga", "almóndiga"));
 
 		assertFalse(anagrama.comprobarAnagrama("Python", "pythoN"));
 	}
 	
-//add test parametrizado
+	@ParameterizedTest
+	@CsvSource({
+		"roma, amor",
+		"copia, opiac",
+		"arroz, zorra",
+		"casa, saca",
+		"snake, ekans",
+		"caso, saco"
+	})
+	public void anagramaParameterizedTestTrue(String str1, String str2) {
+		Anagrama ana = new Anagrama();
+
+		assertEquals(true, ana.comprobarAnagrama(str1, str2));
+	}
+	@ParameterizedTest
+	@CsvSource({
+		"roma, amorr",
+		"roma, romA",
+		"albóndiga, almóndiga",
+		"Python, pythoN"
+	})
+	public void anagramaParameterizedTestFalse(String str1, String str2) {
+		Anagrama ana = new Anagrama();
+
+		assertFalse(ana.comprobarAnagrama(str1, str2));
+	}
 }
