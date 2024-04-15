0f221ce15ab1a67629ed4ee0343c75ca2db3318b
michaelBedDev <micama2000@gmail.com>
2024-04-15 09:02:33 +0200

    Cambio de nombre repo

--- 3av/ZZCD2023/test/test/AnagramaTest.java
+++ 3av/ZZCD2023/test/test/AnagramaTest.java
@@ -9,33 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import ejercicio2.Anagrama;
 
 class AnagramaTest {
-	Anagrama anagrama = new Anagrama();
 	
-	@Test
-	public void testValidaAnagrama() {
-		
-		assertTrue(anagrama.comprobarAnagrama("roma", "amor"));
-		
-		assertTrue(anagrama.comprobarAnagrama("copia", "opiac"));
-		
-		assertTrue(anagrama.comprobarAnagrama("arroz", "zorra"));
-
-		assertTrue(anagrama.comprobarAnagrama("casa", "saca"));
-
-		assertTrue(anagrama.comprobarAnagrama("snake", "ekans"));
-
-		assertTrue(anagrama.comprobarAnagrama("pato", "topa"));
-
-		assertTrue(anagrama.comprobarAnagrama("caso", "saco"));
-        
-		assertFalse(anagrama.comprobarAnagrama("roma", "amorr"));
-        
-		assertFalse(anagrama.comprobarAnagrama("roma", "romA"));
-        
-		assertFalse(anagrama.comprobarAnagrama("albóndiga", "almóndiga"));
-
-		assertFalse(anagrama.comprobarAnagrama("Python", "pythoN"));
-	}
 	
 	@ParameterizedTest
 	@CsvSource({
